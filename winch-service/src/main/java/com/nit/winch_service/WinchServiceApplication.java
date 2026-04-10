8bcc047a3d33e5bc68ceb0fbad751734f6ec19c6
rite10 <ritesh0pradhan@gmail.com>
2026-04-10 19:34:00 +0530

    added new winch-service

---
+++ winch-service/src/main/java/com/nit/winch_service/WinchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nit.winch_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinchServiceApplication.class, args);
+	}
+
+}
---
+++ winch-service/src/test/java/com/nit/winch_service/WinchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nit.winch_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
