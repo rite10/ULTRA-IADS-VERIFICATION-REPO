0aa55d4a5dc227d1cb01f8d3903cd5d6555f2790
rite10 <ritesh0pradhan@gmail.com>
2026-04-10 18:52:54 +0530

    add all microservices

---
+++ iads-eureka-server/src/main/java/com/nit/eureka/IadsEurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.nit.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class IadsEurekaServerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(IadsEurekaServerApplication.class, args);
+	}
+
+}
---
+++ iads-eureka-server/src/test/java/com/nit/eureka/IadsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nit.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IadsEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
